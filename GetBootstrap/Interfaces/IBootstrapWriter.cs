d5c57172b8e66d938c0fbf6f0261571f429aae56
Leonel Sarmiento <Leonel.Sarmiento@infor.com>
2018-01-19 14:48:34 +0800

    Refactor SOLID

---
+++ GetBootstrap/Interfaces/IBootstrapWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Customization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Interfaces
+{
+    public interface IBootstrapWriter
+    {
+        BootstrapType Debug(string format);
+        BootstrapType Pass(string format);
+        BootstrapType Info(string format);
+        BootstrapType Warn(string format);
+        BootstrapType Fatal(string format);
+        BootstrapType Magenta(string format);
+        BootstrapType Cobalt(string format);
+    }
+}
--- GetBootstrap/Program.cs
+++ GetBootstrap/Program.cs
@@ -7,70 +7,24 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GetBootstrap
+namespace System
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Version buildver = Assembly.GetEntryAssembly().GetName().Version;
             Assembly assembly = Assembly.GetExecutingAssembly();
             FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fvi.FileVersion;
+            string releasever = fvi.FileVersion;
 
-            Console.Title = $"GetBootstrap v{version}";
-            Bootstrap.Logger = Logger.GetLogger("GetBootstrap");
-            Bootstrap.Write("DEVELOPER:", BootstrapType.Info, BootsrapStyle.Alert);
-            Bootstrap.Write( " Leonel Sarmiento - ");
-            Bootstrap.WriteLine("Leonel.Sarmiento@outlook.com", BootstrapType.Success);
-            Bootstrap.Write("BOOTSTRAP:", BootstrapType.Magenta, BootsrapStyle.Alert);
-            Bootstrap.WriteLine($" Build {Assembly.GetEntryAssembly().GetName().Version}");
-            Bootstrap.Write("BOOTSTRAP:", BootstrapType.Magenta, BootsrapStyle.Alert);
-            Bootstrap.WriteLine($" Release {version}");
-            Console.ReadKey();
-            
-            Bootstrap.WriteLine("Customize Progress Bar", type: BootstrapType.Info);
+            Console.Title = "GetBootstrap";
 
-            ProgressBar pb = new ProgressBar(Enum.GetNames(typeof(BootstrapType)).Length * 3);
-            pb.Width = 50;
-            pb.DrawProgressBar();
-            
-            ProgressBar pb1 = new ProgressBar(Enum.GetNames(typeof(BootstrapType)).Length * 2);
-            pb1.ProgressColor = ConsoleColor.DarkBlue;
-            pb1.Width = 75;
-            pb1.DrawProgressBar();
-
-            ProgressBar pb2 = new ProgressBar(Enum.GetNames(typeof(BootstrapType)).Length * 1);
-            pb2.ProgressColor = ConsoleColor.DarkMagenta;
-            pb2.Width = 100;
-            pb2.DrawProgressBar();
-
-            Bootstrap.WriteLine("Adjustable Typewriter Speed", type: BootstrapType.Info);
-
-            foreach (var name in Enum.GetNames(typeof(BootstrapType)))
-            {
-                pb.Increment();
-                pb1.Increment();
-                pb2.Increment();
-                Typewriter.WriteLine($"{name}", style: BootsrapStyle.Alert, type: (BootstrapType)Enum.Parse(typeof(BootstrapType), name), fill: true);
-            }
-
-            Bootstrap.WriteLine("Light and Dark Themes", type: BootstrapType.Info);
-
-            foreach (var name in Enum.GetNames(typeof(BootstrapType)))
-            {
-                pb.Increment();
-                pb1.Increment();
-                Typewriter.Write($"{name}", style: BootsrapStyle.Alert, type: (BootstrapType)Enum.Parse(typeof(BootstrapType), name), fill: true);
-            }
-
-            Console.WriteLine();
-
-            foreach (var name in Enum.GetNames(typeof(BootstrapType)))
-            {
-                pb.Increment();
-                Bootstrap.Write($"{name}", type: (BootstrapType)Enum.Parse(typeof(BootstrapType), name));
-            }
-            Console.ReadLine();
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.Warn("Hello, World");
+            Bootstrap.Write("Hello, World");
+            Typewriter.Write("Test");
+            Console.Read();
         }
     }
 }
